d1d229cd517f09dc631decdab788557cb681654b
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-14 11:38:34 +0300

    Optimized product model

--- shoectober/product/models.py
+++ shoectober/product/models.py
@@ -14,10 +14,11 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(blank=True, null=True)
     image = models.ImageField(upload_to="product_image/")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_date = models.DateField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
