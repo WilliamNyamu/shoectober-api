92aa825ced662b41ec01691f96847f5d572a0475
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-14 11:54:51 +0300

    fixed authentication checks in post crud operations

---
+++ shoectober/product/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsCreatororReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only the creator to edit or delete a product
+    """
+    def has_object_permission(self, request, view, obj):
+        # If it is a GET method, then just return true and give the person access
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # If it is an UNSAFE method like POST, PATCH, DELETE etc, ensure this is enforced.
+        return obj.creator == request.user
